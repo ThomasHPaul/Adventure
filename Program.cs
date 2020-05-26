08705e92e80e5bbc6c026aebd5784b8fdbd67b97
ThomasHPaul <tpaul19@wgu.edu>
2020-05-26 12:33:37 -0500

    Refactor from Main to Game class. Added Dialog method.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿/*
+ * Created by: Thomas Paul
+ * 
+ * Date: 5/20/2020
+ * 
+ * This work is a derivative of 
+ * "C# Adventure Game" by http://programmingisfun.com, used under CC BY.
+ * https://creativecommons.org/licenses/by/4.0/
+ */
+
+using System;
 
 namespace Adventure
 {
-    class Program
+    class Game
     {
-        static void Main()
+        static string characterName = "Stalwart Pal";
+
+        public static void StartGame()
         {
-            string characterName = "Stalwart Pal";
+            NameCharacter();
+
+            Console.WriteLine("Your adventure is about to begin!\n\n"
+                + "You have traveled many weeks with a merchant caravan and find yourself in the land of Sinnom. "
+                + "The land is hot and dry, its people seem equally callous and most don't even notice your arrival. "
+                + "Those who look your way do not see you but appear gaze through you. "
+                + "Yoo too begin to feel the weight of the world resting itself on your back. "
+                + "The sight of the inn you are bound for brings no relief.\n"
+                + "Slowly, you walk inside.");
+
+            Console.WriteLine("You sit down at the bar as a man walks toward you. Clearing his throat, he asks");
+            Dialog("Have you see my footpad? That little sneak is around here somewhere. He's half your height and wears tattered leather armor...");
+            Console.Write("How do you answer?\n 1) Yes\n 2) No\n 3) Repeat the question\n 4) Stay silent\n");
+        }
+
+        static void NameCharacter()
+        {
             Console.WriteLine("Welcome to Quest!\n\nWhat is your name?");
 
             string possibleName = Console.ReadLine();
@@ -16,16 +44,31 @@ namespace Adventure
             }
 
             Console.WriteLine($"\nYour character's name is {characterName}.\n\n");
-            
-            Console.WriteLine("Your adventure is about to begin!\n\n"
-                            + "You have traveled many weeks with a merchant caravan and find yourself in the land of Sinnom. "
-                            + "The land is hot and dry, its people seem equally callous and most don't even notice your arrival. "
-                            + "Those who look your way do not see you but appear gaze through you. "
-                            + "Yoo too begin to feel the weight of the world resting itself on your back. "
-                            + "The sight of the inn you are bound for brings no relief.\n"
-                            + "Slowly, you walk inside. ");
+        }
 
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+    }
+
+    class Item
+    {
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Game.StartGame();
             Console.ReadKey();
         }
     }
 }
+
+
+
